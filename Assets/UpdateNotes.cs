8d3f93224dc5435a904acfadc0d918e663cbde6a
egemen6k <egemen.altikardesler@gmail.com>
2021-10-24 13:59:26 +0300

    +UI changes

--- Assets/UpdateNotes.cs
+++ Assets/UpdateNotes.cs
@@ -1,10 +1,11 @@
 ﻿/*
     +++ *Zıpladığı zaman colliderin da değişiklik göstermesi gerekiyor, fizik sıçıyor.
+    +++*highest score fonksiyonu eklenebilir
+    +++*score fonksiyonu eklenecek, geçen zamana göre artacak. Gamemanager singleton olup skoru tutacak.
+
     *Slide animasyon düzeltilmesi gerekiyor
     *post processing & curve shader uygulanabilir
     *müzik eklenmesi gerekiyor
     *ana menü eklenip sceneler arası geçiş yapılacak
     *geçişlerde zorluk seviyesi fonksiyonu eklenebilir
-    *highest score fonksiyonu eklenebilir
-    *score fonksiyonu eklenecek, geçen zamana göre artacak. Gamemanager singleton olup skoru tutacak.
 */
